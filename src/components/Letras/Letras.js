e80ef563db9d5c525f80ef324ffb08dcd1ae5528
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-15 02:11:06 -0300

    create folder Letras

--- src/components/Letras/Letras.js
+++ src/components/Letras/Letras.js
@@ -1,2 +1,30 @@
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+import {Keyboard} from "./style"
+
+export default function Letras(enabledKeyboard, disabledKeys, enableKey) {
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
                   "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    return (
+        <Keyboard>
+            {alfabeto.map((letter, position) => (
+                <Key
+                    key = {position}
+                    letter = {letter}
+                    disabled = {!enabledKeyboard || disabledKeys.includes(position)}
+                    onClick = {() => enableKey(position, alfabeto[position])}
+                />
+            ))}
+        </Keyboard>
+    );
+
+    function Key({disabled, onClick, letter}) {
+        return (
+            <button
+                disabled = {disabled}
+                onClick = {onClick}
+                data-test = "letter">
+                {letter.toUpperCase()}
+            </button>
+        );
+    };
+}
---
+++ src/components/Letras/style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Keyboard = styled.div`
+    width: 665px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 12px;
+
+    button {
+        width: 40px;
+        height: 40px;
+        background-color: #E1ECF4;
+        border: 1px solid #7AA7C7;
+        border-radius: 3px;
+        cursor: pointer;
+        
+        font-family: "Roboto", sans-serif;
+        font-size: 16px;
+        font-weight: 700;
+        color: #39739D;
+
+        &:disabled {
+            background-color: #9FAAB5;
+            color: #798A9F;
+            cursor: default;
+        }
+    }
+`;

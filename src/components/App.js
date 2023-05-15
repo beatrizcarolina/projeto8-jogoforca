bc0846ba12b6f7fc422bd8dbf01f4da00a9fed8a
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-15 15:54:02 -0300

    implementation check game

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,8 @@ export default function App() {
     const [enabledKeyboard, setEnabledKeyboard] = React.useState(false);
     const [disabledKeys, setDisabledKeys] = React.useState([]);
     const [points, setPoints] = React.useState(0);
+    let updateErrors = errorsNumber;
+    let updatePoints = points;
 
     function selectLetters(letter) {
         const newLetters = Array(letter.length).fill(" _");
@@ -27,9 +29,64 @@ export default function App() {
         setWord(chosenWord);
         selectLetters(chosenWord);
     }
+
+
+    function verifyGame()
+    {
+        console.log("Numero Acertos: " + updatePoints);
+        console.log("Numero Erros: " + updateErrors);
+        
+        if(updatePoints === (word.length))
+        {
+            console.log("Venceu");
+            setGameState("win");
+            setEnabledKeyboard(false);
+        }
+        else
+        {
+            if(updateErrors === 6)
+            {
+                console.log("Perdeu");
+                setEnabledKeyboard(false);
+                setGameState("loose");
+            }
+        }
+        seterrosNumber(updateErrors);
+        setPoints(updatePoints);
+    }
+
+    function incrementError() {
+		updateErrors++;
+	}
+
+    function incrementPoints() {
+		updatePoints++;
+	}
+
+    function verifyLetter(letter){
+        if(word.includes(letter))
+        {
+            const newLetters = letters;
+            for(var i = 0; i < word.length;i++){
+                if(word[i] === letter)
+                {
+                    newLetters[i] = letter;
+                    incrementPoints();
+                }
+            }
+            setLetters(newLetters);
+        }
+        else
+        {
+            incrementError();
+        }
+
+    };
     
-    function enableKey(position) {
+    function enableKey(position,letter) {
         setDisabledKeys([...disabledKeys, position]);
+        verifyLetter(letter);
+        verifyGame();
     };
     
     function startGame() {

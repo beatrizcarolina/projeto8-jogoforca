cc91585e551e2cac1fb815761a403fe283df289c
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-05-15 01:05:31 -0300

    deleted folders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-import "./css/reset.css";
-import "./css/style.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-    <React.StrictMode>
-        </App>
-    </React.StrictMode>
-);
+ReactDOM.render(<App/>, document.querySelector(".root"));
